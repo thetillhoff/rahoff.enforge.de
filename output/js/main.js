a509359a5a76ec8d380f34e3fffbe5804ea75802
Till Hoffmann <till.hoffmann@enforge.de>
2021-04-01 18:59:57 +0200

    add current (initial) state - mobile first

--- output/js/main.js
+++ output/js/main.js
@@ -1,11 +1,25 @@
 window.onload = function() {
 
+  /* DIMMER */
+  document.getElementById("overlay-loader").classList.add("hidden");
+
   /* BUTTONS */
   var buttonsByTag = document.getElementsByTagName("button");
   var buttonsByClass = document.getElementsByClassName("button");
   var buttons = [ ...buttonsByTag, ...buttonsByClass];
   for (var i = 0; i < buttons.length; i++) {
-    console.log("found a button!")
+    console.log("found a button!");
   }
   
+}
+
+/* toggle navigation menu and page contents */
+function togglePageNavigation() {
+  if (document.getElementById("pageNavigation").classList.contains("hidden")) {
+    document.getElementById("page").classList.add("hidden");
+    document.getElementById("pageNavigation").classList.remove("hidden");
+  } else {
+    document.getElementById("pageNavigation").classList.add("hidden");
+    document.getElementById("page").classList.remove("hidden");
+  }
 }
