f9ac65222ffb4ae48e095bb59e3ab6909768449c
Till Hoffmann <till.hoffmann@enforge.de>
2021-05-09 00:10:08 +0200

    removed devcontainer - too complicated currently

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-git clone https://github.com/tillhoff/temingo /workspaces/temingo
-cd /workspaces/temingo && task build
-
-git clone https://github.com/tillhoff/serve /workspaces/serve
-cd /workspaces/serve && task build
