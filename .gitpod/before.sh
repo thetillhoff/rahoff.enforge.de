522402ad0873c64692d9d9b2b6b192e21f205573
tillhoff <till.hoffmann@enforge.de>
2021-05-09 02:46:59 +0200

    gitpod tests

--- .gitpod/before.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ! -f /workspace/serve/serve ]]; then
-  git clone https://github.com/tillhoff/serve /workspace/serve && cd /workspace/serve && task build
-fi
-
-if [[ ! -f /workspace/temingo/temingo ]]; then
-  git clone https://github.com/tillhoff/temingo.. /workspace/temingo && cd /workspace/temingo && task build
-fi
