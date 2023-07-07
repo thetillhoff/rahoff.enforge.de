2e3738e092b3a65f8f9d7bab5db1643f8d428172
Till Hoffmann <till@thetillhoff.de>
2023-07-07 20:40:00 +0200

    Updated even more versions

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,20 @@
 import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-      edge: false,
-      split: false
-    })
-	},
-  files: {
-    lib: "/src/lib"
-  }
-};
+export default {
+    kit: {
+        // default options are shown
+        adapter: adapter({
+            // if true, will create a Netlify Edge Function rather
+            // than using standard Node-based functions
+            edge: false,
 
-export default config;
+            // if true, will split your app into multiple functions
+            // instead of creating a single one for the entire app.
+            // if `edge` is true, this option cannot be used
+            split: false
+        })
+    },
+    files: {
+        lib: "/src/lib"
+    }
+};
